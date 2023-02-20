d1d53dc766644c658bba05745eb737fcf6f8dc4c
Snehal979 <bansodsnehal535@gmail.com>
2023-02-20 21:46:14 +0530

    [Snehal]Add: Given message

---
+++ MoodAnalyserProgramBatch/MoodAnalyser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProgramBatch
+{
+    public class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message=message;
+        }
+        /// <summary>
+        /// Uc1 Given Message and Return it
+        /// </summary>
+        /// <returns></returns>
+        public string CheckMoodAnalyser()
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }       
+}
--- MoodAnalyserProgramBatch/Program.cs
+++ MoodAnalyserProgramBatch/Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Mood Analyser Program");
+            MoodAnalyser moodAnalyser = new MoodAnalyser("Iam in Happy mood");
+            Console.WriteLine(moodAnalyser.CheckMoodAnalyser());
             Console.ReadLine();
         }
     }
